aabe62a8c3d97c632aa8fa17d0bef7b938e22c9b
jishnu-vydurya <jishnu@vydurya.in>
2026-02-02 16:12:44 +0530

    Fix image case sensitivity issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -83,7 +83,7 @@ export default function CoffeeHero() {
         {/* RIGHT IMAGE */}
         <div className="w-full lg:w-1/2 mt-12 lg:mt-0 flex justify-center">
           <img
-            src="/images/Hero2.webp"
+            src="/images/hero2.webp"
             alt="Coffee Pour"
             className="max-w-sm sm:max-w-md lg:max-w-lg "
           />
